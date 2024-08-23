4e67ebd83c1bc51ba2960184373b657605fcbfba
bushraBhutto <bushra.bhutto246@gmail.com>
2024-08-23 17:13:49 +0500

    quote generator

---
+++ script.js
@@ -0,0 +1,45 @@
+// //creating variabls
+// let btn=document.querySelector('#new-quote');
+// let quote=document.querySelector('.quote');
+// let person=document.querySelector('.person');
+// const quotes=[
+//     {
+//         quote:"the best way to find yourself is to lose yourself in the services of others",
+//         person: "Mahtama Gandhi"
+//     },{
+//         quote:"if you want to live a happy life, tie it to a goal,not to people or things",
+//         person:"Albert Einstein"
+//     },{
+//         quote:"at his best, man is the noblest of all animals;seperated from law and justice he is the worst",
+//         person:"Aristotle"
+//     },{
+//         quote:"your time is limited, so don't waste it living someone else's life",
+//         person: "steve jobs"
+//     },{
+//         quote: "tell me and I forget teach me and I remember. Involve me and I learn",
+//         person: "Banjamin franklin"
+//     },
+//     {
+//         quote:"If you look at what you have in life, you'll always have more if you look at what you don't have in life,you'll never have enough",
+//         person:'oprah winfrey'
+//     },{
+//         quote: "it does not matter matter how slowly you go as you do not stop",
+//         person:"confucuis"
+//     },{
+//         quote: "our lives began to end the day we become silient about things that matters",
+//         person:"Martin Luther king jr"
+//     },
+//     {
+//         quote:"Remember that not getting what you want is sometimes a wonderful stroke of luck",
+//         person: "Dalai lama"
+//     },
+//     {
+//         quote:"the journey of a thousand miles begins with one stem",
+//         person:"Lao tzu"
+//     }
+// ];
+// btn.addEventListener("click",function(){
+//     let random=Math.floor(Math.random()*quotes.length);
+//     quote.innerText=quotes[random].quote;
+//        person.innerText=quotes[random].person;
+// })
